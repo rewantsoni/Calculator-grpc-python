3d6de9ad7335f9f7141f45f2b8d8d700c745f909
Rewant <rewant.soni@gmail.com>
2019-07-05 14:45:42 +0530

    Trying circuit Breaker

--- client.py
+++ client.py
@@ -1,12 +1,48 @@
 import grpc
 import calculator_grpc_pb2
 import calculator_grpc_pb2_grpc
+import pybreaker
 
+breaker = pybreaker.CircuitBreaker(fail_max=2, reset_timeout=2)
+@breaker
+def run(client, n):
+    if n == '1':
+        print('Welcome to Addition')
+        x = int(input('Enter First num:'))
+        y = int(input('Enter Second num:'))
+        # raise pybreaker.CircuitBreakerError
+        print('here')
+        res = client.add(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
+        print(res.num)
+    elif n == '2':
+        print('Welcome to Subtraction')
+        x = int(input('Enter First num:'))
+        y = int(input('Enter Second num:'))
+        res = client.sub(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
+        # tme.sleep(5)
+        print(res.num)
+    elif n == '3':
+        print('Welcome to Multiplication')
+        x = int(input('Enter First num:'))
+        y = int(input('Enter Second num:'))
+        res = client.mul(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
+        print(res.num)
+    elif n == '4':
+        print('Welcome to Division')
+        x = int(input('Enter First num:'))
+        y = int(input('Enter Second num:'))
+        res = client.div(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
+        print(res.num)
+    elif n == '5':
+        print('Exiting')
+        exit()
+    else:
+        print('Invalid Input,')
 
-if __name__ == '__main__':
-    channel = grpc.insecure_channel('localhost:8081')
+@breaker
+def connect():
+    channel = grpc.insecure_channel('localhost:9999')
     client = calculator_grpc_pb2_grpc.apiStub(channel)
-
     while True:
         print('Enter 1 to add,'
               ' 2 to subtract,'
@@ -14,32 +50,14 @@ if __name__ == '__main__':
               ' 4 to divide, 5 to exit')
         print()
         n = input('Enter your choice: ')
-        if n == '1':
-            print('Welcome to Addition')
-            x = int(input('Enter First num:'))
-            y = int(input('Enter Second num:'))
-            res = client.add(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
-            print(res.num)
-        elif n == '2':
-            print('Welcome to Subtraction')
-            x = int(input('Enter First num:'))
-            y = int(input('Enter Second num:'))
-            res = client.sub(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
-            print(res.num)
-        elif n == '3':
-            print('Welcome to Multiplication')
-            x = int(input('Enter First num:'))
-            y = int(input('Enter Second num:'))
-            res = client.mul(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
-            print(res.num)
-        elif n == '4':
-            print('Welcome to Division')
-            x = int(input('Enter First num:'))
-            y = int(input('Enter Second num:'))
-            res = client.div(calculator_grpc_pb2.twoNums(numOne=x, numTwo=y))
-            print(res.num)
-        elif n =='5':
-            print('Exiting')
-            exit()
-        else:
-            print('Invalid Input,')
+        try:
+            running = run(client, n)
+            print(running)
+        except pybreaker.CircuitBreakerError:
+            print('here')
+            print(pybreaker.CircuitBreakerError)
+
+
+if __name__ == '__main__':
+    connect()
+
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import calculator_grpc_pb2_grpc
 def serve():
     grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     calculator_grpc_pb2_grpc.add_apiServicer_to_server(CalculatorServicer(), grpc_server)
-    grpc_server.add_insecure_port('[::]:8081')
+    grpc_server.add_insecure_port('[::]:9999')
     grpc_server.start()
     while True:
         time.sleep(860000)
@@ -20,6 +20,7 @@ class CalculatorServicer(calculator_grpc_pb2_grpc.apiServicer):
         return calculator_grpc_pb2.num(num=(request.numOne+request .numTwo))
 
     def sub(self, request, context):
+        print('here')
         return calculator_grpc_pb2.num(num=(request.numOne-request .numTwo))
 
     def mul(self, request, context):
